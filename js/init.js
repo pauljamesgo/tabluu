f7a5c11b2472cdf46d0b2726acab9ddd9cdb2151
pauljamesgo <paul@thickthinfat.com>
2015-02-06 00:10:16 +0800

    working on navigation mobile

--- js/init.js
+++ js/init.js
@@ -1,18 +1,15 @@
-$(w).resize(function(){ //Update dimensions on resize
-  sw = document.documentElement.clientWidth;
-  sh = document.documentElement.clientHeight;
-  checkMobile();
+$(function() {
+	var push = $('#push');
+		menu = $('#navigation ul');
+		menuHeight = menu.height();
+		$(push).on('click', function(e) {
+			e.preventDefault();
+			menu.slideToggle();
+		});
+		$(window).resize(function(){
+	       	var w = $(window).width();
+	       	if(w > 320 && menu.is(':hidden')) {
+	       	menu.removeAttr('style');
+    	}
+	});
 });
-  
-//Check if Mobile
-function checkMobile() {
-  mobile = (sw > breakpoint) ? false : true;
-
-  if (!mobile) { //If Not Mobile
-    $('[role="tabpanel"],.nav,.login').show(); //Show full navigation and search
-  } else { //Hide 
-    if(!$('#nav-anchors a').hasClass('active')) {
-      $('.nav,.login').hide(); //Hide full navigation and search
-    }
-  }
-}
